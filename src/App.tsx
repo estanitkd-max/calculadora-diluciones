657d36e4c7c46ab61d134ffd0b7c9cb470465143
Estanislao <estani.tkd@gmail.com>
2026-03-25 13:04:43 -0300

    tamaño imagen

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,8 @@ export default function App() {
 
       // 2. DEFINIMOS EL TAMAÑO MÁXIMO QUE QUEREMOS EN LA FICHA
       // (Podés cambiar estos números si querés el logo más grande o chico)
-      const anchoMaximo = 200; 
-      const altoMaximo = 100;
+      const anchoMaximo = 300; 
+      const altoMaximo = 200;
 
       // 3. CALCULAMOS EL TAMAÑO FINAL MANTENIENDO LA PROPORCIÓN
       // (Esta es la parte "mágica" para que no se vea aplastado)
